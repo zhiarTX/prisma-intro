5aec9fd64d0debeecd85fb540dbe7be71a5bc4b3
zhiarTX <138315070+zhiarTX@users.noreply.github.com>
2023-11-02 11:07:42 -0600

    Update seed.js
    Created a function to create 20 authors with 3 books each

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,24 @@
 const prisma = require('../prisma');
-const seed = async () => {
-  // TODO: Create 20 authors with 3 books each
+
+// This function is used to create 20 authors with 3 books each. It then creates
+// an array of books for each author.
+const seed = async (numAuthors = 20, booksPerAuthor = 3) => {
+  for (let i = 0; i < numAuthors; i++) {
+    const books = [];
+    for(let j = 0; j < numAuthors; j++) {
+      books.push({title: `Book ${i}${j}` });
+    }
+    await prisma.author.create({
+      data: {
+        name: `Author ${i}`,
+        books: {
+          create: books,
+        },
+      },
+    });
+  }
 };
+
 seed()
   .then(async () => await prisma.$disconnect())
   .catch(async (e) => {
